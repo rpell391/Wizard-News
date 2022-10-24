983e8b51a3f124cfc0e56e2c7700d20cbf88b50c
Randy Pellegrin <rpell391@gmail.com>
2022-10-24 14:05:15 -0500

    setup complete

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello World!"));
-
+app.use(morgan("dev"));
 const PORT = 1337;
 
 app.listen(PORT, () => {
