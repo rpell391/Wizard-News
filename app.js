f0c685ea263432a49b9016dd21487025c4e1e370
Brevan <brev.bdawg@gmail.com>
2022-10-25 10:54:07 -0500

    data listed, routing issues unresolved

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
+const postBank = require("./postBank");
 
+// const routes = require("./routes");
+const app = express();
+
+app.use('/static', express.static('public'));
+// app.use(routes);
 app.use(morgan("dev"));
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  
+const posts = postBank.list();
+
+const html = `<!DOCTYPE html>
+<html>
+  <head>
+    <title>Wizard News</title>
+  </head>
+  <body>
+    <ul>
+      ${posts.map(post => `<li>${post.title}</li>`)}
+    </ul>
+  </body>
+</html>`;
+
+
+res.send(html)
+});
+
 const PORT = 1337;
 
 app.listen(PORT, () => {
