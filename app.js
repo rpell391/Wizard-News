0a484bf6c8ef015fd4c67256c5f5059d43e18cbb
Randy Pellegrin <rpell391@gmail.com>
2022-10-25 09:39:15 -0500

    end of class

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
 
-app.get("/", (req, res) => res.send("Hello World!"));
 app.use(morgan("dev"));
+app.get("/", (req, res) => res.send("Hello World!"));
 const PORT = 1337;
 
 app.listen(PORT, () => {
