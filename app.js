c74e5d187e8313bcdc733a778fb2d755eb240dbe
Brevan <brev.bdawg@gmail.com>
2022-10-25 12:12:05 -0500

    lunch break commit

--- app.js
+++ app.js
@@ -5,24 +5,38 @@ const postBank = require("./postBank");
 // const routes = require("./routes");
 const app = express();
 
-app.use('/static', express.static('public'));
+app.use(express.static('public'));
 // app.use(routes);
 app.use(morgan("dev"));
 app.get("/", (req, res) => {
   
 const posts = postBank.list();
 
-const html = `<!DOCTYPE html>
+const html = 
+`<!DOCTYPE html>
 <html>
   <head>
     <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css"/>
   </head>
   <body>
-    <ul>
-      ${posts.map(post => `<li>${post.title}</li>`)}
-    </ul>
+    <div class="newsList">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+      <div class='newsItem'>
+       <p>
+        <span class="newsPosition">${post.id}. ▲</span>${post.title}
+        <small>(by ${post.name}</small>
+        </p> 
+        <small class="newsInfo">
+          ${post.upvotes} upvotes | ${post.date}
+        </small>
+        </div>`
+      ).join('')}
+      </div>
+    
   </body>
-</html>`;
+</html>;`
 
 
 res.send(html)
