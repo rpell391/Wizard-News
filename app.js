d2eb038460fb70b4bc6c3c818bfbc6e3302d83b3
Brevan <brev.bdawg@gmail.com>
2022-10-27 10:44:19 -0500

    final commit time ago working

--- app.js
+++ app.js
@@ -4,21 +4,11 @@ const postBank = require("./postBank");
 
 const timeAgo = require("node-time-ago");
 
-timeAgo(new Date()); // just now
-timeAgo(Date.now()); // just now
-timeAgo(new Date().toISOString()); // just now
+// timeAgo(new Date()); // just now
+// timeAgo(Date.now()); // just now
+// timeAgo(new Date().toISOString()); // just now
 
-timeAgo(Date.now() + 35 * 1000); // "35 second ago",
-timeAgo(Date.now() + 65 * 1000); // "a minute ago",
-timeAgo(Date.now() + 3 * 60 * 1000); // "3 minutes ago",
-timeAgo(Date.now() + 65 * 60 * 1000); // "an hour ago",
-timeAgo(Date.now() + 3 * 60 * 60 * 1000); // "3 hours ago",
-timeAgo(Date.now() + 25 * 60 * 60 * 1000); // "a day ago",
-timeAgo(Date.now() + 3 * 24 * 60 * 60 * 1000); // "3 days ago",
-timeAgo(Date.now() + 35 * 24 * 60 * 60 * 1000); // "a month ago",
-timeAgo(Date.now() + 3 * 30 * 24 * 60 * 60 * 1000); // "3 months ago",
-timeAgo(Date.now() + 13 * 30 * 24 * 60 * 60 * 1000); // "a year ago",
-timeAgo(Date.now() + 3 * 12 * 30 * 24 * 60 * 60 * 1000); // "3 years ago"
+
 // const routes = require("./routes");
 const app = express();
 
@@ -46,7 +36,7 @@ app.get("/", (req, res) => {
         <small>(by ${post.name}</small>
         </p> 
         <small class="newsInfo">
-          ${post.upvotes} upvotes | ${post.date}
+          ${post.upvotes} upvotes | ${timeAgo(post.date)}
         </small>
         </div>`
         )
@@ -97,7 +87,7 @@ app.get("/posts/:id", (req, res) => {
             <small>(by ${post.name}</small>
             </p> 
             <small class="newsInfo">
-            ${post.date}
+            ${timeAgo(post.date)}
             </small>
             <span>
              ${post.content}
